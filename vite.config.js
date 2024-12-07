f1b495e3fd2e37572516ce59eb8c80fd0b5c7b31
Ahmed-DS-ML <ahmed.datascince@gmail.com>
2024-12-07 23:53:43 +0200

    Fix MIME types, PWA icons, and asset loading issues

--- vite.config.js
+++ vite.config.js
@@ -20,7 +20,21 @@ export default defineConfig({
           vendor: ['react', 'react-dom', 'react-router-dom'],
           animations: ['framer-motion', 'react-type-animation'],
           ui: ['@headlessui/react', '@heroicons/react']
-        }
+        },
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split('.');
+          const extType = info[info.length - 1];
+          if (/\.(png|jpe?g|svg|gif|tiff|bmp|ico)$/i.test(assetInfo.name)) {
+            return `assets/images/[name].[hash][extname]`;
+          }
+          if (/\.css$/i.test(assetInfo.name)) {
+            return `assets/css/[name].[hash][extname]`;
+          }
+          if (/\.(woff2?|eot|ttf|otf)$/i.test(assetInfo.name)) {
+            return `assets/fonts/[name].[hash][extname]`;
+          }
+          return `assets/[name].[hash][extname]`;
+        },
       }
     }
   },
